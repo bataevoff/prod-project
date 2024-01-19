5e08c81f4f8a081d39f9819e00c0a688c0ddd4a0
bataevoff <85867199+bataevoff@users.noreply.github.com>
2024-01-19 10:52:12 +0100

    added theme toggle and context

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useContext, useState } from "react";
 import Counter from "./components/Counter";
-import "./index.scss";
+import "./styles/index.scss";
 import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Theme, ThemeContext } from "./theme/ThemeContext";
+import { useTheme } from "./theme/useTheme";
 
 function App() {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
       <Link to={"/about"}>About</Link>
       <Link to={"/"}>Main</Link>
+      <button onClick={toggleTheme}>Toggle</button>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path={"/"} element={<MainPageAsync />} />
