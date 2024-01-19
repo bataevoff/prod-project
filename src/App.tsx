7e44325c280589660d3c967656e525c9d1b477d0
bataevoff <85867199+bataevoff@users.noreply.github.com>
2024-01-19 11:10:07 +0100

    added helper classNames

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React, { Suspense, useContext, useState } from "react";
+import { Suspense } from "react";
 import Counter from "./components/Counter";
 import "./styles/index.scss";
 import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { Theme, ThemeContext } from "./theme/ThemeContext";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/ClassNames";
 
 function App() {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames("app", {}, [theme])}>
       <Link to={"/about"}>About</Link>
       <Link to={"/"}>Main</Link>
       <button onClick={toggleTheme}>Toggle</button>
